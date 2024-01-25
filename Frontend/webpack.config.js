c8b3fb49b7675597be71a9c1d043f37aa99b769a
Marcelo Abbadia <marceloabbadia@Marcelos-MacBook-Pro.local>
2024-01-25 09:47:53 -0300

    Feat: Webpack and Babel configuration for the frontend

---
+++ Frontend/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'public'),
+    "hot":true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
